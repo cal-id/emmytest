1dbf74d1555a27a538c2fcf4f4eb0b8d8c6b4810
callum-i <callum.iddon@stfc.ac.uk>
2017-05-10 15:24:21 +0100

    Inital commit for ZKP without gRPC
    This doesn't actually work because pedersenReceiver is not exported

---
+++ ZKP/ZKP.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+    "log"
+    "github.com/xlab-si/emmy/common"
+    "github.com/xlab-si/emmy/dlogproofs"
+    "math/big"
+
+)
+
+
+
+/*
+ * Attempt at implementing an ZKP without using GRPC
+ */
+func main(){
+    var protocolType common.ProtocolType = common.ZKPOK
+    // Other options are listed here for the future
+    // protocolType = common.Sigma
+    // protocolType = common.ZKP
+    // protocolType = common.ZKPOK
+
+    // Print the protocol type being used
+    log.Println("Using this protocol type: ", protocolType)
+
+
+    // Create the verifier directly (dlogproofs.NewSchnorrProtocolServer is
+    // just a struct containing this verifier and the protocolType)
+    verifier := dlogproofs.NewSchnorrVerifier(protocolType)
+
+    // Create the prover directly (dlogproofs.NewSchnorrProtocolClient is just
+    // a struct that includes the prover and the GRPC connection information
+    // alongside the protocolType)
+    prover, err := dlogproofs.NewSchnorrProver(protocolType)
+    // Prover has: DLog
+    if err != nil {
+        log.Fatalf("error when creating Schnorr prover: %v", err)
+    }
+    log.Println("Prover.DLog: ", *prover.DLog)
+
+    // IN MY NOTES, an P before a variable means that that variable is used
+    // exclusively with the pedersen committer / receiver.
+    // Variables g, p, q are shared between the pedersen and the sigma
+
+    // --------------------- Vars and their meaning ---------------------------
+
+    // SHARED
+    // p                                                                      ?
+    // q                                              the order of the subgroup
+    // g                                                          the generator
+
+    // PEDERSEN COMMITTER / RECEIVER
+    // Pa                                            trapdoor created by client
+    // Ph = g^Pa        the shared value which forces the client to stick to Pa
+    // Px                                    the commited value = the challenge
+    // Pr                       a random value to be revealed when decommitting
+    // Pc = g^Px * Ph^Pr               the commitment to Px before revealing Px
+
+    // SIGMA ONLY
+    // r                                             random number to send over
+    // s                              secret = the key only known to the client
+    // x = g ^ r                                         known as the challenge
+    // t = g ^ s                                          considered public key
+
+
+    // -------------------- Equivalent of OpeningMsg() ------------------------
+    // Prover should first set up the group
+
+    // CLIENT
+    // Ph = g^Pa where Pa is a trapdoor (the commitment)
+    // p
+    // g = generator for the group
+    // q is the order of the subgroup
+    Ph, p, q, g := prover.GetOpeningMsg()
+    // Normally {H:Ph, P:p, OrderOfSubgroup:q, G:g} -> server
+
+    // SERVER
+    // store 'received' values in both the pedersen committer and the verifier
+    verifier.SetCommitmentGroup(p, q, g)
+    verifier.SetGroup(p, q, g)
+    // Store Ph in the verifer,
+    // get the challenge (== committed value)
+    // and return the commitment, Pc = g^Px * Ph^Pr
+    // Pr is a random
+    // Px is the commited value
+    Pc := verifier.GetOpeningMsgReply(Ph) // Pc
+    // Normally {X1: commitment} -> client
+
+    // CLIENT
+    // Store the commitment to check later
+    prover.pedersenReceiver.SetCommitment(Pc)
+
+    // ----------------- Equivalent of ProofRandomData() ----------------------
+    // CLIENT
+    // Create a secret
+    s := big.NewInt(345345345334)
+    // Store the secret and store a random number r
+    // Return:
+    // x = g ^ r
+    // t = g ^ s
+    x, t := prover.GetProofRandomData(s)
+    // Normally {X:x, P:p, OrderOfSubgroup:q, G:g, T:t} -> server
+
+    // SERVER
+    verifier.SetProofRandomData(x, t)  // store in verifier
+    // This line reveals the committed value, Px from the server to the client
+    // It reveals the random value to prove that it committed to this value
+    // earlier
+    Px, Pr := verifier.GetChallenge()
+    // Normally {X: Px, R:Pr} -> client
+
+    // CLIENT
+    // Check that Px is the value that was committed to using Pr in the first
+    // message response.
+    // In other words, show that Px and Pr produce Pc (Pc was stored in
+    // SetCommitment(commitment))
+    success := prover.pedersenReceiver.CheckDecommitment(Pr, Px)
+    if ! success{
+        // If the decommitment failed then something went wrong.
+        log.Fatalf("Commitment failed")
+    }
+
+    // ---------------------- Equivalent of ProofData() -----------------------
+    // CLIENT
+    // z = r + Px * s
+    // Pa = trapdoor, the original Pa from OpeningMsg()
+    z, Pa := prover.GetProofData(Px)
+    // Normally {Z:z, Trapdoor:Pa} -> server
+
+    // SERVER
+    // Check that the pedersen committer verifies the trapdoor against h
+    // Also check that g^z == t^Px * x
+    // The RHS of that equation is: (g^s)^Pa * g^r
+    valid := verifier.Verify(z, Pa)
+
+    if ! valid{
+        log.Fatalf("Trapdoor or z was not verified")
+    } else {
+        log.Println("Worked!")
+    }
+
+}
---
+++ ZKP/ZKP.md
@@ -0,0 +1,52 @@
+# Notes on ZKP
+
+gRPC sets up a client-server relationship where the client starts every message and the server returns a response
+
+1. "Not client" creates `schnorrServer := dlogproofs.NewSchnorrProtocolServer(protocolType)`
+    * Creates `verifier := dlogproofs.NewSchnorrVerifier(protocolType)`
+        * Creates `pedersenCommitter = commitments.NewPedersenCommitter()`
+            * Returns `PedersenCommitter` struct
+        * Returns `SchnorrVerifier` struct with `pedersenCommitter` initilised
+    * Returns `verifier` and `protocolType` in a struct as `SchnorrProtocolServer`
+2. "Not client" calls `schnorrServer.Listen()`
+    * Registers `SchnorrProtocolServer` to the GPRC with `pb.RegisterSchnorrProtocolServer(s, server)`
+        * Registers `"OpeningMsg"`
+            * Takes `PedersenFirst`: {H, P, OrderOfSubgroup, G}
+            * Returns `BigInt`: {X1}
+        * Registers `"ProofRandomData"` handled by `_SchnorrProtocol_ProofRandomData_Handler`
+            * Takes `SchnorrProofRandomData`: {X, P, OrderOfSubgroup, G, T}
+            * Returns `PedersenDecommitment`: {X, R}
+        * Registers `"ProofData"` handled by `_SchnorrProtocol_ProofData_Handler`
+            * Takes `SchnorrProofData`: {Z, Trapdoor}
+            * Returns `Status`: {Success}
+3. "Client" creates `schnorrProtocolClient, err := dlogproofs.NewSchnorrProtocolClient(protocolType)`
+    * Sets up the grpc connection
+    * Creates `client := pb.NewSchnorrProtocolClient(conn)`
+        * Returns a struct with client connection
+    * Creates `prover, err := NewSchnorrProver(protocolType)`
+    * Returns `protocolClient` struct with {client, conn, prover, protocolType}
+4. "Client" creates `secret := big.NewInt(345345345334)`
+5. "Client" calls run `isProved, err := schnorrProtocolClient.Run(secret)`
+    * Runs OpeningMsg `commitment, _ := client.OpeningMsg()`
+        * gets h, p, q, g using `client.prover.GetOpeningMsg()` (stores as `msg`)
+        * sends it to the server using `(*client.client).OpeningMsg(context.Background(), msg)`
+            * server sets group `s.verifier.SetCommitmentGroup(p, q, g)`
+            * server sets group again `s.verifier.SetGroup(p, q, g)`
+            * Returns the commitment `commitment := s.verifier.GetOpeningMsgReply(h)`
+        * Returns the commitment
+    * Stores the commitment `client.prover.pedersenReceiver.SetCommitment(commitment)`
+        * Literally provides access to private variable
+    * Sends first message of sigma and stores challenge `challenge, r, err := client.ProofRandomData(secret)`
+        * Stores x and t `x, t := client.prover.GetProofRandomData(secret)`
+        * Sends to the server (with previous p, q, G)`reply, err := (*client.client).ProofRandomData(context.Background(), msg)`
+            * Sever stores: `s.verifier.SetProofRandomData(x, t)`
+            * Returns challenge `challenge, r2 := s.verifier.GetChallenge()`
+        * Returns challenge = `reply.X`, r2 = `reply.R`
+    * Checks decommitment was valid `success = client.prover.pedersenReceiver.CheckDecommitment(r, challenge)`
+        * Literally
+    * Sends trapdoor and receives result `proved, err := client.ProofData(challenge)`
+        * Generate trapdoor `z, trapdoor := client.prover.GetProofData(challenge)`
+        * Send to server `(*client.client).ProofData(context.Background(), msg))`
+            * Check if valid `valid := s.verifier.Verify(z, trapdoor)`
+            * Returns valid
+        * Returns valid
